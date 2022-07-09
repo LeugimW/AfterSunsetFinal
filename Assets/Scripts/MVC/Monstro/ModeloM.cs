cc0b1a043ce8589e39f06e39df3ff750e8ad24ff
LeugimW <miguel.ramos.mota1@gmail.com>
2022-07-09 00:33:10 -0500

    Version Final

---
+++ Assets/Scripts/MVC/Monstro/ModeloM.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ModeloM : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public float timermov = 7;
+
+    public AudioSource ChaseTheme;
+    public AudioClip musica_tension;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkPoint;
+    public bool walkPointSet;
+    public float walkPointRange;
+
+    public float sightRange;
+    public bool playerInSightRange;
+}
